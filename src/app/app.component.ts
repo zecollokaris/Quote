fc53c76d6fb4ea3295121962f860682f171625f8
zecollokaris <collo.kariss@gmail.com>
2018-03-19 17:29:36 +0300

    initial save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  quotes:string[];
+  constructor(){
+    
+    this.quotes = ["A Stictch","Buy cookies","Phonecase"];
+  }
 }

0bb50434572402c034bef0c0bd4c200c8aa46c48
zecollokaris <collo.kariss@gmail.com>
2018-03-19 19:56:52 +0300

    started adding toogle

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,8 @@
 
 export class quote {
-  constructor(public id: number, public name:string,public description:string){}
+  public showDescription:boolean;
+  constructor(public id: number, public name:string,public description:string){
+    this.showDescription=false
+  }
 }
 

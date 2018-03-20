b44f2e9c03afbbdc02bec6f773ddd2844b061574
zecollokaris <collo.kariss@gmail.com>
2018-03-20 10:35:02 +0300

    end of pipe exampple MAKING OWN CUSTOM PIPE

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new quote(1, '1aaaaabbbbbb', 'aaaaaaa', new Date(2018, 3, 14)),
-    new quote(2, '1cccccbbbbbb', 'aaaaaaa', new Date(2018, 3, 14)),
-    new quote(3, '1ddddddabbbbbb', 'aaaaaaa', new Date(2018, 3, 14)),
-    new quote(4, '1eeeeeeeabbbbbb', 'aaaaaaaa', new Date(2018, 3, 14)),
-    new quote(5, '1ffffffbbbbbb', 'aaaaaaaa', new Date(2018, 3, 14)),
-    new quote(6, '1gggggggbbbbbb', 'aaaaaaa', new Date(2018, 3, 14))
+    new quote(1, '1aaaaabbbbbb', 'aaaaaaa', new Date(2018, 2, 18)),
+    new quote(2, '1cccccbbbbbb', 'aaaaaaa', new Date(2018, 2, 18)),
+    new quote(3, '1ddddddabbbbbb', 'aaaaaaa', new Date(2018, 2 19)),
+    new quote(4, '1eeeeeeeabbbbbb', 'aaaaaaaa', new Date(2018, 2, 19)),
+    new quote(5, '1ffffffbbbbbb', 'aaaaaaaa', new Date(2018, 2, 20)),
+    new quote(6, '1gggggggbbbbbb', 'aaaaaaa', new Date(2018, 2, 20))
   ]
 
   completeQuote(isComplete, index) {

e77cb4efbdd0294da309a73ed1a0c25612798f87
zecollokaris <collo.kariss@gmail.com>
2018-03-20 10:54:27 +0300

    THIS IS THE SPECIFIC COMMIT USED TO ADD PIPE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { StrikethroughDirective } from './strikethrough.directive';
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
   quotes = [
     new quote(1, '1aaaaabbbbbb', 'aaaaaaa', new Date(2018, 2, 18)),
     new quote(2, '1cccccbbbbbb', 'aaaaaaa', new Date(2018, 2, 18)),
-    new quote(3, '1ddddddabbbbbb', 'aaaaaaa', new Date(2018, 2 19)),
+    new quote(3, '1ddddddabbbbbb', 'aaaaaaa', new Date(2018, 2, 19)),
     new quote(4, '1eeeeeeeabbbbbb', 'aaaaaaaa', new Date(2018, 2, 19)),
     new quote(5, '1ffffffbbbbbb', 'aaaaaaaa', new Date(2018, 2, 20)),
     new quote(6, '1gggggggbbbbbb', 'aaaaaaa', new Date(2018, 2, 20))

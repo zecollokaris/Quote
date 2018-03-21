cff5657ebce70063740bd60553a13735cd268611
zecollokaris <collo.kariss@gmail.com>
2018-03-21 14:47:00 +0300

    finished styling

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -19,7 +19,7 @@ export class QuoteComponent implements OnInit {
 
   deleteQuote(isComplete, index) {
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].author}`)
 
       if (toDelete) {
         this.quotes.splice(index, 1)

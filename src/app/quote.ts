61ac30d340baf2a3296ca41675fb6d8ef03cab8a
zecollokaris <collo.kariss@gmail.com>
2018-03-21 15:18:29 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-
-export class quote {
-  public showDescription: boolean;
-  constructor(public id: number, public author: string, public description: string, public completeDate: Date) {
-    this.showDescription = false
-  }
-}
